85913779aa6c38d776703586b0589325db8f322f
Tapan Das <tapands.das@gmail.com>
2023-02-11 11:28:56 +0530

    Add render module
    It will display attack mark

--- src/components/dom.js
+++ src/components/dom.js
@@ -1,4 +1,5 @@
 import playSession from "./index";
+import render from "./render";
 
 const humanBoard = playSession.humanPlayer.ownBoard;
 const cpuBoard = playSession.cpuPlayer.ownBoard;
@@ -8,20 +9,24 @@ const cpuBoardCells = document.querySelectorAll(".cpu-board > div");
 
 function parseCoordinates(cell) {
   const cellClasses = cell.classList;
-  console.log(cellClasses[0]);
   const col = cellClasses[0].split("-")[0];
   const row = Number(cellClasses[0].split("-")[1]);
-  console.log(col, row);
+  return { col, row };
 }
 
 humanBoardCells.forEach((cell) => {
   cell.addEventListener("click", () => {
     console.log(cell);
-    cell.classList.add("attacked-cell");
-    const attackMark = document.createElement("div");
-    attackMark.classList.add("attack-mark");
-    cell.appendChild(attackMark);
-    parseCoordinates(cell);
+
+    // cell.classList.add("attacked-cell");
+    // const attackMark = document.createElement("div");
+    // attackMark.classList.add("attack-mark");
+    // cell.appendChild(attackMark);
+
+    render(cell);
+    const point = { ...parseCoordinates(cell) };
+    humanBoard.recieveAttack(point);
+    console.log(humanBoard);
   });
 });
 
---
+++ src/components/render.js
@@ -0,0 +1,6 @@
+export default function render(cell) {
+  cell.classList.add("attacked-cell");
+  const attackMark = document.createElement("div");
+  attackMark.classList.add("attack-mark");
+  cell.appendChild(attackMark);
+}
