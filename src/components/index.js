45864ac582cb3f95924321166cca6cd8602565db
Tapan Das <tapands.das@gmail.com>
2023-02-13 13:02:13 +0530

    Add the footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import favicon from "../assets/favicon.png";
+import githubIcon from "../assets/github-icon.png";
 
 import session from "./session";
 import { humanPlayerFactory, cpuPlayerFactory } from "./player";
@@ -11,6 +12,9 @@ link.setAttribute("rel", "shortcut icon");
 link.setAttribute("type", "image/x-icon");
 link.setAttribute("href", favicon);
 head.appendChild(link);
+
+const imageTag = document.querySelector(".footer > a img");
+imageTag.src = githubIcon;
 
 const user = humanPlayerFactory();
 const cpu = cpuPlayerFactory();
